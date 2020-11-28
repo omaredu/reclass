cd755d4bd58504bfab1746ddd9f9bf253e2ab696
Sofia <doradodx@gmail.com>
2020-11-28 13:30:16 -0600

    movement correct

--- Red/Assets/Scripts/PlayerMovement.cs
+++ Red/Assets/Scripts/PlayerMovement.cs
@@ -4,32 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController characterController;
-    public float MovementSpeed = 1;
-    public float Gravity = 9.8f;
-    private float velocity = 0;
+    public CharacterController controller;
 
-    private void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-    }
-
+    public float speed = 12f;
     void Update()
     {
-        // player movement - forward, backward, left, right
-        float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
-        float vertical = Input.GetAxis("Vertical") * MovementSpeed;
-        characterController.Move((transform.right * horizontal + transform.forward * vertical) * Time.deltaTime);
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        // Gravity
-        if (characterController.isGrounded)
-        {
-            velocity = 0;
-        }
-        else
-        {
-            velocity -= Gravity * Time.deltaTime;
-            characterController.Move(new Vector3(0, velocity, 0));
-        }
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
