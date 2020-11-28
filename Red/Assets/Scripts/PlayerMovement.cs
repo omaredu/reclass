44ca618da33c85d5c42c0c1a2b3ffdeecbc103da
Sofia <doradodx@gmail.com>
2020-11-28 16:08:42 -0600

    modelo example

--- Red/Assets/Scripts/PlayerMovement.cs
+++ Red/Assets/Scripts/PlayerMovement.cs
@@ -8,19 +8,27 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 8f;
 
-    //SIT DOWN
-    public GameObject sitDownUi;
+    public bool walk = true;
 
-    public GameObject silla;
+    public bool once = false;
+
+    public bool sitDown = false;
+
+    //SIT DOWN
+    public GameObject sitDownUi, modeloUi;
+
 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        if (walk == true)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+            Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+            controller.Move(move * speed * Time.deltaTime);
+        }
     }
 
     // silla collider
@@ -30,6 +38,10 @@ public class PlayerMovement : MonoBehaviour
         {
             sitDownUi.SetActive(true);
         }
+        else if (other.CompareTag("modelo"))
+        {
+            modeloUi.SetActive(true);
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -38,6 +50,17 @@ public class PlayerMovement : MonoBehaviour
         {
             Debug.Log("nice");
             this.transform.parent = other.transform;
+            this.transform.position = new Vector3(2.2f, 1.79f, -3.37f);
+            walk = false;
+            sitDown = true;
+            StartCoroutine(sitDownSeconds());
+        }
+        if (Input.GetKey(KeyCode.Q) && sitDown == true)
+        {
+            Debug.Log("out");
+            this.transform.position = new Vector3(0.58f, 2.81f, -3.07f);
+            walk = true;
+            sitDown = false;
         }
     }
 
@@ -45,4 +68,12 @@ public class PlayerMovement : MonoBehaviour
     {
         sitDownUi.SetActive(false);
     }
+
+    private IEnumerator sitDownSeconds()
+    {
+        yield return new WaitForSeconds(1f);
+        once = true;
+    }
 }
+
+
--- Red/Assets/Scripts/SitDown.cs
+++ Red/Assets/Scripts/SitDown.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class SitDown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject sitDownUI;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
         
     }
