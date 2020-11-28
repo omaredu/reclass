8ea66fe74e0a00433b343a0dfc49df57d76c6e16
Sofia <doradodx@gmail.com>
2020-11-28 12:44:50 -0600

    mouse and player

--- Red/Assets/Scripts/PlayerMovement.cs
+++ Red/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    CharacterController characterController;
+    public float MovementSpeed = 1;
+    public float Gravity = 9.8f;
+    private float velocity = 0;
+
+    private void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // player movement - forward, backward, left, right
+        float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
+        float vertical = Input.GetAxis("Vertical") * MovementSpeed;
+        characterController.Move((Vector3.right * horizontal + Vector3.forward * vertical) * Time.deltaTime);
+
+        // Gravity
+        if (characterController.isGrounded)
+        {
+            velocity = 0;
+        }
+        else
+        {
+            velocity -= Gravity * Time.deltaTime;
+            characterController.Move(new Vector3(0, velocity, 0));
+        }
     }
 }
