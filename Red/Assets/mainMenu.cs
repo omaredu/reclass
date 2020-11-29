c9dd30ba44281be2789695a896ce770e5fa13f1e
Sofia <doradodx@gmail.com>
2020-11-29 11:17:15 -0600

    menu principal

---
+++ Red/Assets/mainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public int next_level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        next_level = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    public void nextLevel()
+    {
+        SceneManager.LoadScene(next_level);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
